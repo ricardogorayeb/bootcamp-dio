0fb68fb4f875664b6ee0b5401dfdd58c5543503d
ricardogorayeb <ricardogorayeb@gmail.com>
2021-04-28 13:49:11 -0400

    Mensagem de exibição do menu.

--- bootcampkotlin/app/src/main/java/com/example/bootcampkotlin/MainActivity.kt
+++ bootcampkotlin/app/src/main/java/com/example/bootcampkotlin/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.bootcampkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -46,10 +49,28 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater : MenuInflater = menuInflater
         inflater.inflate(R.menu.menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_menu_1 -> {
+                    showToast("Exibindo item de Menu 1")
+                    true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Exibindo item de Menu 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 
