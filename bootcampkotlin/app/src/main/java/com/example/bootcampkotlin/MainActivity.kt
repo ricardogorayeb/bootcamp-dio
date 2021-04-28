354b50d256e479ed274073987b889d55804982bf
ricardogorayeb <ricardogorayeb@gmail.com>
2021-04-28 14:56:36 -0400

    Criação do menu do tipo Drawer.

--- bootcampkotlin/app/src/main/java/com/example/bootcampkotlin/MainActivity.kt
+++ bootcampkotlin/app/src/main/java/com/example/bootcampkotlin/MainActivity.kt
@@ -6,9 +6,14 @@ import android.os.Message
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.bootcampkotlin.R.id.toolbar
 
 class MainActivity : AppCompatActivity() {
     private val rvList: RecyclerView by lazy {
@@ -19,12 +24,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
+        initDrawer()
         bindViews()
         updateList()
     }
 
+    private fun initDrawer() {
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
+        val toogle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+
+    }
 
     private fun bindViews() {
         rvList.adapter = adapter
