cf19e3b202d3863ba88a6d8a1c014252366409c9
ricardogorayeb <ricardogorayeb@gmail.com>
2021-04-28 10:28:48 -0400

    Projeto Lista de Contatos commit inicial.

---
+++ bootcampkotlin/app/src/main/java/com/example/bootcampkotlin/Contact.kt
@@ -0,0 +1,8 @@
+package com.example.bootcampkotlin
+
+data class Contact {
+    var name: String,
+    var phone: String,
+    var photograph: String
+
+}
--- bootcampkotlin/app/src/main/java/com/example/bootcampkotlin/ContactAdapter.kt
+++ bootcampkotlin/app/src/main/java/com/example/bootcampkotlin/ContactAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.bootcampkotlin
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.view.menu.ActionMenuItemView
+import android.widget.TextView
+import androidx.core.content.contentValuesOf
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
@@ -10,19 +12,36 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
     private val list: MutableList<Contact> = mutableListOf()
 
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item,parent, false)
+        return ContactAdapterViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(list[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
+    }
+
+    fun updateList(list: List<Contact>) {
+        this.list.clear()
+        this.list.addAll(list)
+        motifyDataSetChanged()
     }
 
     class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val tvName: TextView = itemView.findViewById(R.id.iv_name)
+        private val tvPhone: TextView = itemView.findViewById(R.id.iv_phone)
+        private val tvPhotograph: TextView = itemView.findViewById(R.id.iv_photograph)
+
+        fun bind(contact: Contact) {
+            tvName.text = contact.name
+            tvName.text = contact.phone
+        }
 
     }
 }
+
