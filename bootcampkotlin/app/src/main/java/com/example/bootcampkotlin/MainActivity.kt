708caf3e70cbe65809fad6fe148abe6035fc824f
ricardogorayeb <ricardogorayeb@gmail.com>
2021-04-28 10:29:49 -0400

    Projeto Lista de Contatos commit inicial.

---
+++ bootcampkotlin/app/src/main/java/com/example/bootcampkotlin/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.bootcampkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+
+    private val adapter = ContactAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bindViews()
+    }
+
+    private fun bindViews() {
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+
+    }
+}
